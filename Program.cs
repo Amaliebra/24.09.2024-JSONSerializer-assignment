64668a93161c855f413df425874996c8a2158837
amaliebra <jba082024@gmail.com>
2024-09-24 11:08:02 +0200

    streamreader

--- Program.cs
+++ Program.cs
@@ -3,16 +3,25 @@ using System.IO;
 
 namespace _24._09._2024_JSONSerializer_assignment
 {
-    internal class Program
+    class Program
     {
         public static void Main()
         {
-            string path = @"C:\Users\amali\Desktop\School\24.09.2024-JSONSerializer-assignment\temp";
+            string path = @"C:\Users\amali\Desktop\School\24.09.2024-JSONSerializer-assignment\temp\MyText.txtdo";
             if (!File.Exists(path))
             {
                 using (StreamWriter sw = File.CreateText(path))
                 {
-                    sw.WriteLine("HELLO WORLD")
+                    sw.WriteLine("HELLO WORLD");
+                }
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
                 }
             }
         }
