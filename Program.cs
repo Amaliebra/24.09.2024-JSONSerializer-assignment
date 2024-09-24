75245ccc59b8d3973277c8f4f0fd189a4d0613ce
amaliebra <jba082024@gmail.com>
2024-09-24 10:45:56 +0200

    edit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.IO;
+
+namespace _24._09._2024_JSONSerializer_assignment
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\amali\Desktop\School\24.09.2024-JSONSerializer-assignment\temp";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("HELLO WORLD")
+                }
+            }
+        }
+    }
+}
